7f5846347e37014ec51511dee461a23800033d62
pareshgithub <pareshgadre2012@gmail.com>
2020-08-25 20:37:36 -0400

    code added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'observabledemo';
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    let foo: any = new Observable((observer) => {
+      let counter = 0;
+      setInterval(() => {
+        observer.next(counter);
+        counter++;
+      }, 1000);
+      setTimeout(() => {
+        console.log('complete')
+        observer.complete();
+      }, 7000);
+      
+    });
+
+    foo.subscribe((data) => {
+      console.log(data);
+    })
+
+    
+    
+  }
 }
